aed9acdfbfae18c3a5ee269716b6b5bec3138f05
aphy18 <aphason1@gmail.com>
2023-05-10 11:26:32 -0400

    starting routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// import pages
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ProductDetails from './pages/ProductDetails';
+
+// import components
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return <div className='overflow-hidden'>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/shop" element={<Shop />} /> 
+      </Routes>
+      <Footer />
+    </Router>
+  </div>;
 };
 
 export default App;
