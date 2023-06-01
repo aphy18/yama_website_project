1f59acf08a9abf5673db18154f6ec3ed3963fb12
aphy18 <aphason1@gmail.com>
2023-06-01 09:35:46 -0400

    may continue later

--- frontend/src/contexts/ProductContext.js
+++ frontend/src/contexts/ProductContext.js
@@ -1,17 +1,17 @@
-import React, { createContext, useState, useEffect } from 'react';
+// import React, { createContext, useState, useEffect } from 'react';
 
-export const ProductContext = createContext();
+// export const ProductContext = createContext();
 
-const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const fetchProducts = async() => {
-      const res = await fetch();
-    }
-  }, [])
-  return <ProductContext.ProductProvider>
-    {children}
-  </ProductContext.ProductProvider>
-};
+// const ProductProvider = ({ children }) => {
+//   const [products, setProducts] = useState([]);
+//   useEffect(() => {
+//     const fetchProducts = async() => {
+//       const res = await fetch();
+//     }
+//   }, [])
+//   return <ProductContext.ProductProvider>
+//     {children}
+//   </ProductContext.ProductProvider>
+// };
 
-export default ProductContext;
+// export default ProductContext;
