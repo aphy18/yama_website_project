47cfea650bb29d1452f35645f2ec46d7de1974cc
aphy18 <aphason1@gmail.com>
2023-06-01 10:03:20 -0400

    creating space for product display

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product }) => {
+  //destructure product
+  console.log('product from products page:', product)
+  const { id, sold_out, image } = product;
+  return (
+    <div>
+      <div className='border border-2 border-black h-[300px] m-4 relative overflow-hidden group transition'>
+
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Product from '../components/Product';
 import axios from 'axios';
 const api = `http://localhost:8080`;
 
@@ -19,9 +20,9 @@ const Home = () => {
         <div>
           <section className='py-16'>
             <div className='container mx-auto'>
-              <div className='flex flex-col lg: flex-row md:flex-row max-w-sm mx-auto'> 
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl-grid-cols-5'> 
                 {products.map(product => {
-                  return <div className='w-full h-[300px] bg-pink-200 mb-4' key={product.id}>{product.name}</div>
+                  return <Product product={product} key={product.id} />
                 })}
               </div>
             </div>
