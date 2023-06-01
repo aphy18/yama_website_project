ded8ac6e3088088b63a9564c6bb250b8240f76da
aphy18 <aphason1@gmail.com>
2023-06-01 09:48:10 -0400

    working on home

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -10,12 +10,23 @@ const Home = () => {
     useEffect(() => {
       const fetchData = async () => {
         const result = await axios.get(api);
-        console.log('result -->', result);
+        console.log('result -->', result.data);
+        setProducts(result.data);
       }
       fetchData();
     }, [])
     return (
-        <p>Hello world</p>
+        <div>
+          <section className='py-16'>
+            <div className='container mx-auto'>
+              <div className='flex flex-col lg: flex-row md:flex-row max-w-sm mx-auto'> 
+                {products.map(product => {
+                  return <div className='w-full h-[300px] bg-pink-200 mb-4' key={product.id}>{product.name}</div>
+                })}
+              </div>
+            </div>
+          </section>
+        </div>
     )
   };
   
