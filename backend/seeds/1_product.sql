03f19eba30a77399a993bfc95e0aee461db4780b
aphy18 <aphason1@gmail.com>
2023-06-08 13:54:50 -0400

    additional columns to db

--- backend/seeds/1_product.sql
+++ backend/seeds/1_product.sql
@@ -1,4 +1,4 @@
-INSERT INTO product (name, quantity, sold_out, image) VALUES ('anime girl', 3, FALSE, ''); 
-INSERT INTO product (name, quantity, sold_out, image) VALUES ('anime boy', 10, FALSE, ''); 
-INSERT INTO product (name, quantity, sold_out, image) VALUES ('keychain', 5, FALSE, ''); 
-INSERT INTO product (name, quantity, sold_out, image) VALUES ('jacket', 1, FALSE, ''); 
+INSERT INTO product (name, category, quantity, price, sold_out, image) VALUES ('anime girl', 'art', 3, 12, FALSE '../images/joker.jpg'); 
+INSERT INTO product (name, category, quantity, price, sold_out, image) VALUES ('anime boy', 'art', 10, 25, FALSE, '../images/bang.png'); 
+INSERT INTO product (name, category, quantity, price, sold_out, image) VALUES ('keychain', 'accessory', 5, 8,  FALSE, '../images/monkey.png'); 
+INSERT INTO product (name, category, quantity, price, sold_out, image) VALUES ('jacket', 'clothing', 1, 25,  FALSE, '../images/piece.jpg'); 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -9,26 +9,33 @@ import joker from '../images/joker.jpg';
 const Product = ({ product }) => {
   //destructure product
   console.log('product from products page:', product)
-  const { id, sold_out, image } = product;
+  const { id, name, quantity, sold_out, image } = product;
   
   return (
     <div>
       <div className='border border-2 border-[#e4e4e4] h-[300px] m-4 relative overflow-hidden group transition'>
         <div className='w-full h-full flex justify-center items-center'>
           {/* Add to Cart */}
-          <div className='absolute top-6 -right-11 bg-red-500/40 p-2 opacity-0 flex flex-col items-center gap-y-2 group-hover:opacity-100 group-hover:right-5 transition-all duration-300'>
+          <div className='absolute top-6 -right-11 bg-red-500/40 p-2 opacity-0 flex flex-col items-center gap-y-2 group-hover:opacity-100 group-hover:right-1 transition-all duration-300'>
             <button>
               <div className='flex justify-center items-center text-white w-12 h-12 bg-red-500'>
                 <BsPlus className='text-3xl'></BsPlus>
               </div>
             </button>
-            <Link to={'/'} className='w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl'>
+            <Link to={`/product/${id}`} className='w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl'>
               <BsEyeFill />
             </Link>
           </div>
           {/* image */}
-          <img className='max-h-[160px] group-hover:scale 110 transition duration-300' src={joker} alt='' />
+          <img className='max-h-[160px] group-hover:scale-110 transition-all duration-300' src={joker} alt='' />
         </div>
+      </div>
+      {/* product info */}
+      <div className='border-2 border-solid border-black h-auto'>
+      <Link to={`/product/${id}`}>
+        <h2 className='font-semibold mb-1'>{name}</h2>
+
+      </Link>
       </div>
     </div>
   );
