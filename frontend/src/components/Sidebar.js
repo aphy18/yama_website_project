9722bc2180845787c2a95d7f8971f91c3425b766
aphy18 <aphason1@gmail.com>
2023-06-15 00:21:17 -0400

    finished cart context

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -8,6 +8,7 @@ import CartItem from '../components/CartItem';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
+  const { cart } = useContext(CartContext)
   console.log(useContext(CartContext))
 
   return <div className={`${isOpen ? 'right-0' : '-right-full'} w-full border-2 bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px[35px] bg-red-500 `}>
@@ -15,6 +16,9 @@ const Sidebar = () => {
       <div className='uppercase text-sm font-semibold'>Shopping Bag (0)</div>
       <div onClick={() => handleClose()} className='cursor-pointer w-8 h-8 flex justify-center items-center'><IoIosArrowRoundForward className="text-4xl" /></div>
     </div>
+    <div>{cart.map(item => {
+      return <CartItem item={item} key={item.id} />
+    })}</div>
   </div>;
 };
 
