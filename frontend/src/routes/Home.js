c3264f8f98dcdab35590f1751306b6397cb2b160
aphy18 <aphason1@gmail.com>
2023-06-15 00:42:01 -0400

    updated shop route

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,34 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import Product from '../components/Product';
-import axios from 'axios';
-const api = `http://localhost:8080`;
+import React from 'react'
 
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
 
-
-const Home = () => {
-  const [products, setProducts] = useState([])
-
-    useEffect(() => {
-      const fetchData = async () => {
-        const result = await axios.get(api);
-        console.log('result -->', result.data);
-        setProducts(result.data);
-      }
-      fetchData();
-    }, [])
-    return (
-        <div>
-          <section className='py-16'>
-            <div className='container mx-auto border-2 border-solid border-red-500'>
-              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl-grid-cols-5'> 
-                {products.map(product => {
-                  return <Product product={product} key={product.id} />
-                })}
-              </div>
-            </div>
-          </section>
-        </div>
-    )
-  };
-  
-export default Home;
+export default Home
--- frontend/src/routes/Shop.js
+++ frontend/src/routes/Shop.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Product from '../components/Product';
+import axios from 'axios';
+const api = `http://localhost:8080`;
+
 
 const Shop = () => {
-  return <div>Shop</div>;
-};
+  const [products, setProducts] = useState([])
 
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios.get(api);
+        console.log('result -->', result.data);
+        setProducts(result.data);
+      }
+      fetchData();
+    }, [])
+    return (
+        <div>
+          <section className='py-16'>
+            <div className='container mx-auto border-2 border-solid border-red-500'>
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl-grid-cols-5'> 
+                {products.map(product => {
+                  return <Product product={product} key={product.id} />
+                })}
+              </div>
+            </div>
+          </section>
+        </div>
+    )
+  };
+  
 export default Shop;
