044b72ca48918f918b68e3d22fb886ee67ba0d24
aphy18 <aphason1@gmail.com>
2023-07-16 19:35:00 -0400

    commit

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='border-2 border-solid border-black w-full h-[100vh] flex flex-col justify-center items-center'>
+        <section className='border-2 border-solid border-red-500 w-full h-[70%] flex justify-center items-center'>
+          <h1 className='text-[85px]'>Welcome to Y.A.M.A.</h1>
+        </section>
+        <section className='border-2 border-solid border-blue-400 w-full h-[30%]'>
+
+        </section>
+    </div>
   )
 }
 
