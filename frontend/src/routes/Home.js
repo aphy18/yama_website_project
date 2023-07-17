c031e5db2cdc073b0a774b909d2e184a6c2a4b0c
aphy18 <aphason1@gmail.com>
2023-07-16 23:27:57 -0400

    working on social links

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -8,7 +8,15 @@ function Home() {
           <h1 className='lg:text-[60px] text-[40px] bg-black text-white px-40 py-6 rounded-xl my-20'>Welcome to Y.A.M.A.</h1>
         </section>
         <section className='border-2 border-solid border-blue-400 w-full h-1/3 flex justify-evenly'>
-        <div className='border-2 border-solid border-black w-[33.3%] h-full'></div>
+        <div className='border-2 border-solid border-black w-[33.3%] h-full flex flex-col justify-start items-center'>
+          <h3 className='text-[25px]'>Social Media:</h3>
+            <ul className='text-[22px] border-2 border-solid border-green-300 h-full w-full flex flex-col justify-evenly items-start'>
+              <li><a href='https://discord.com/invite/4xtzBerzXH'>Discord</a></li>
+              <li><a href='https://www.instagram.com/yorkuyama/'>Instagram</a></li>
+              <li><a href='https://www.tiktok.com/@yorkuyama'>Tiktok</a></li>
+              <li><a href='https://www.facebook.com/groups/YorkUYAMA'>Facebook</a></li>
+            </ul>
+        </div>
         <div className='border-2 border-solid border-black w-[33.3%] h-full'></div>
         <div className='border-2 border-solid border-black w-[33.3%] h-full'></div>
         </section>
