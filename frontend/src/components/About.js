8213c29df24ddebcb8e20080b3ee81f61b446d98
aphy18 <aphason1@gmail.com>
2023-07-29 21:08:45 -0400

    formatting contact information

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -29,7 +29,7 @@ function About() {
 
   return (
     <>
-    <div id="about-us" className='w-full h-auto flex flex-col justify-evenly items-start relative pb-32'>
+    <div id="about-us" className='w-full h-auto flex flex-col justify-evenly items-start relative'>
         <div id="yama-logo" className='lg:w-[500px] lg:h-[425px] bg-yama bg-[length:500px_500px] bg-center fixed transition-all ease duration-200 self-center'></div>
         <section id="first-section" className='z-20 w-full lg:h-auto flex lg:flex-row items-center my-4'>
           <div id="intro-paragraph" className='h-full lg:my-8 text-[22px] flex flex-col justify-center items-center text-center bg-white px-4'>
@@ -40,7 +40,7 @@ function About() {
         </section>
         <div id="second paragraph" className='w-full lg:h-[250px] relative flex justify-center items-center bg-black/70 text-white self-center'>
           {/* <img src={yamaGirl} className='abolute bottom-0 -left-2 lg:w-[300px] h-[300px] z-20 absolute scale-[0.8]' /> */}
-          <div className='lg:w-full lg:h-full flex text-[35px]'>
+          <div className='lg:w-full lg:h-full flex text-[35px] border-white'>
           <div className='w-1/4 h-full flex justify-center items-center transition-all ease duration-200 hover:bg-white/70 hover:text-black'>
             <p>Our Team</p>
             <IoIosCalendar className='text-[50px] ml-4' />
@@ -61,22 +61,22 @@ function About() {
         </div>
         <div id="third paragraph" className='w-full lg:h-[685px] flex justify-between items-center z-20'>
           <div className='w-1/2 h-full flex justify-center items-center bg-tabling bg-contain relative'>
-            <ul id="register-paragraph" className='w-1/2 h-[400px] z-20 text-[18px] flex flex-col justify-evenly items-start list-disc border-2 border-solid border-white/80 relative font-bold px-8 ml-2'>
-            <h3 className='text-[25px] z-20 underline self-center'>Registration</h3>
-              <li>Full School Year Membership - $15</li>
-              <li>Fall / Winter Semester Membership - $10</li>
-              <li>Receive a membership through tablings done throughout the school year</li>
-              <li>Registration Updates Made in the Discord Server</li>
+            <ul id="register-paragraph" className='w-1/2 h-[400px] z-20 text-[18px] flex flex-col justify-evenly items-start list-disc border-2 border-solid border-white/80 relative font-bold px-8 ml-4'>
+              <h3 className='text-[25px] z-20 underline self-center'>Registration</h3>
+              <li>Full Year Membership - <span className='text-red-500'>$15</span></li>
+              <li>Single Semester Membership - <span className='text-red-500'>$10</span></li>
+              <li>Tabling Location: Bear Pit @Central Square</li>
+              <li>Registration Updates Made in our Discord Server!</li>
             </ul>
           </div>
-          <div id="contact-paragraph" className='w-1/2 h-full flex flex-col justify-evenly items-center relative'>
-            <img src={yamaContact} className='scale-[0.5] -z-10' />
-            {/* <h2 className='text-[35px] z-20 font-bold mb-8 underline'>Inquiries</h2>
-            <ul className='border-2 border-blue-500 border-solid w-full h-[400px]'>
-              <li>asasss</li>
-              <li>asasaa</li>
-              <li>dsdsdsd</li>
-            </ul> */}
+          <div id="contact-paragraph" className='w-1/2 h-full flex justify-center items-center relative bg-contact bg-cover bg-black/50'>
+            <ul className='w-3/5 h-4/5 z-20 text-[28px] flex flex-col justify-evenly items-start list-disc relative font-bold px-8 text-white rounded-lg'>
+              <h3 className='text-[35px] z-20 underline self-center'>Inquiries</h3>
+              <li>Email yorkuyamako@gmail.com to address any questions</li>
+              <li>(Phone Number)</li>
+              <li>Club Room #113 Bethune College, Keele Campus</li>
+              <li>Showing Room #320 Bethune</li>
+            </ul>
           </div>
         </div>
     </div>
