cf8bbf3762e630adc719654a179c7ee855d8d6a1
SABA HAILU <sabahailu@SABAs-MacBook-Air.local>
2023-08-29 11:24:35 -0400

    clubphoto section

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -10,10 +10,10 @@ function Home() {
   return (
     <div id="home-container" className='h-auto flex flex-col justify-start items-center'>
         <section id="clubphoto" className='border-2 border-solid border-cyan-500 w-full flex lg:flex-row lg:justify-start lg:items-center h-1/4 flex-col justify-center items-center bg-clubphoto bg-cover bg-center relative'>
-          <div id="yama-welcome-container" className='border-2 border-green-500 border-solid z-20 flex flex-col lg:justify-center lg:items-start lg:w-auto lg:h-full lg:h-[50%] h-[70%] lg:mt-0 w-full justify-start items-center mt-20 mr-0'>
-            <p className='yama-welcome lg:text-[105px] lg:px-20 md:text-[70px] text-[45px] text-white font-[600] font-robot text-red-600'>YAMA Anime and</p>
-            <p className='yama-welcome lg:text-[105px] md:text-[70px] text-[45px] lg:px-20 font-[600] text-red-600'>Manga Association</p>
-            <h2 className='lg:text-[35px] md:text-[40px] text-[20px] text-white lg:px-24 rounded-xl mt-12'>York University 2023</h2>
+          <div id="yama-welcome-container" className='border-2 border-green-500 border-solid z-20 flex flex-col lg:px-20 lg:justify-center lg:items-start lg:w-auto lg:h-full h-[70%] w-full justify-start items-center lg:mt-8 mt-20 mr-0'>
+            <p className='yama-welcome lg:text-[55px] text-[45px] text-white font-[600] font-robot text-red-600'>YAMA Anime and</p>
+            <p className='yama-welcome lg:text-[55px] text-[45px] font-[600] text-red-600'>Manga Association</p>
+            <h2 className='lg:text-[35px] text-[20px] text-white rounded-xl mt-12'>York University 2023</h2>
             <a href="#about-us" className='transition-all duration-300 ease'><button className='lg:self-start rounded-lg lg:text-[17px] text-[16px] lg:w-[130px] md:h-[70px] w-[120px] h-[70px] lg:mx-[100px] lg:my-[10%] my-[20%] border-white border-solid border-2 bg-none text-white flex justify-center items-center rounded-xl transition-all ease duration-200 hover:bg-white hover:text-black my-[5%]'>Explore <IoIosArrowRoundForward className='lg:text-[30px] text-[20px]' /></button></a>
           </div>
           <div id="york-logo-container" className='border-2 border-solid border-white w-[250px] h-full z-20 flex justify-start items-end '>
