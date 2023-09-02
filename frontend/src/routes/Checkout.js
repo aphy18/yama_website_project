6be8b7b5c22a050c6ce977c70342586a596b3370
aphy18 <aphason1@gmail.com>
2023-09-02 14:02:48 -0400

    constructing order form

--- frontend/src/routes/Checkout.js
+++ frontend/src/routes/Checkout.js
@@ -18,8 +18,8 @@ const Checkout = () => {
   if (checkoutCart) {
     return (
       <>
-        <div className='flex flex-col justify-evenly items-center w-full border-2 border-solid border-black'>
-          <section className='w-full h-auto border-2 border-solid border-red-400 flex flex-col justify-around items-center'>
+        <div className='flex flex-col justify-start items-center w-full h-auto border-2 border-solid border-black'>
+          <section className='w-full h-auto border-2 border-solid border-red-400 flex flex-col justify-around items-center mt-20'>
             <h2 className='text-[40px]'>Your Cart:</h2>
             <div className='border-2 border-solid border-blue-500 w-auto flex justify-center items-center flex-wrap'>
               {checkoutCart.map(checkoutItem => {
@@ -33,8 +33,21 @@ const Checkout = () => {
             })}
             </div>
           </section>
-          <section className='border-2 border-solid border-green-400 w-full h-[800px]'>
-
+          <section className='border-2 border-solid border-green-400 w-full h-[800px] flex flex-col justify-evenly items-center'>
+              <h1>Submit Order:</h1>
+              <form className='w-1/2 border-2 border-solid border-black h-[400px] flex'>
+                <div className='w-1/2 h-full border-2 border-solid border-red-400 flex flex-col justify-evenly items-center'>
+                  <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4' placeholder="Name" />
+                  <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4' placeholder="Email" />
+                  <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4' placeholder="Phone Number" />
+                </div>
+                <div className='w-1/2 border-2 border-solid border-red-400 flex flex-col justify-evenly items-center'>
+                  <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4' placeholder="Shipping Address" />
+                  <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4' placeholder="Postal Code" />
+                  <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4 opacity-0 -z-10' placeholder="Postal Code" />
+                </div>
+              </form>
+              <button>Place Order</button>
           </section>
         </div>
       </>
