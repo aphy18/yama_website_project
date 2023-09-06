bce9025441f1398786566e4e23c36152b8e04ed3
aphy18 <aphason1@gmail.com>
2023-09-06 00:45:41 -0400

    commit

--- frontend/src/routes/Checkout.js
+++ frontend/src/routes/Checkout.js
@@ -56,10 +56,10 @@ const Checkout = () => {
   if (checkoutCart) {
     return (
       <>
-        <div id="main-checkout-container" className='flex flex-col justify-start items-center w-full h-auto bg-yamabackground bg-cover p-10 relative border-2 border-solid border-cyan-300'>
+        <div id="main-checkout-container" className='flex flex-col justify-start items-center w-full h-auto bg-yamabackground bg-cover p-10 relative'>
           <section className='big-container w-4/5 h-auto flex flex-col justify-around mt-20 bg-white z-20 px-8 border-[2px] border-solid border-orange-400 border-b-0 overflow-hidden overflow-x-scroll'>
             <h2 className='text-[25px] my-4 sticky top-0 left-0 font-bold'>Total Price: ${totalPrice}</h2>
-            <div id="confirm-cart" className='flex flex justify-evenly items-center border-2 border-solid border-black'>
+            <div id="confirm-cart" className='flex flex justify-evenly items-center'>
               {checkoutCart.map(checkoutItem => {
                 return <div className='w-[200px] h-[200px] flex flex-col justify-between items-center m-4'>
                 <img src={checkoutItem.img_name} className='w-[150px] h-[150px]' />
@@ -79,19 +79,19 @@ const Checkout = () => {
               <span id="almost-done" className='text-[18px] absolute top-4 left-2 text-gray-500 opacity-0 transition-opacity ease duration-500'>Complete order here.</span>
               <h1 className='text-[30px] mt-12'>Submit Order:</h1>
               <form ref={form} onSubmit={sendEmail} className='w-full h-[500px] flex flex-col items-center'>
-                <div className='main-input-container flex w-full flex-col border-2 border-solid border-blue-400'>
-                  <div className='input-container w-full flex flex-col justify-evenly items-center border-2 border-solid border-red-400'>
+                <div className='main-input-container flex w-full flex-col'>
+                  <div className='input-container w-full flex flex-col justify-evenly items-center'>
                     <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4 m-[10px]' name="full_name" placeholder="Name" required/>
                     <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4 m-[10px]' name="email"  placeholder="Email" required/>
                     <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4 m-[10px]' name="phone_number"  placeholder="Phone Number" required/>
                   </div>
-                  <div className='input-container w-full flex flex-col justify-evenly items-center border-2 border-solid border-red-400'>
+                  <div className='input-container w-full flex flex-col justify-evenly items-center'>
                     <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4 m-[10px]' name="address"  placeholder="Shipping Address" required/>
                     <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4 m-[10px]' name="postal_code"  placeholder="Postal Code" required/>
                     <input type="text" className='border-2 border-solid rounded-xl h-[30px] text-[22px] p-4 opacity-0 -z-10 m-[10px]' placeholder="Postal Code" />
                   </div>
                 </div>
-                <button className='w-[130px] h-[70px] bg-orange-500 text-[18px] text-white rounded-xl m-8'>Place Order</button>
+                <button className='w-[130px] h-[70px] bg-orange-500 text-[18px] text-white rounded-xl m-8 transition-all duration-200 ease hover:bg-orange-500'>Place Order</button>
               </form>
             </div>
               <p id="payment-notice" className='absolute bottom-0 m-4 text-[18px] text-gray-500 opacity-0 transition-opacity ease duration-500'>Payments are not processed on the website.</p>
