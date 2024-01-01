24c0b1e9f3e8a97f770f7650356321a75c37a768
aphy18 <aphason1@gmail.com>
2023-12-31 19:09:07 -0500

    commit

---
+++ frontend/src/components/AboutSlideShow.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { MdArrowBackIos,  MdArrowForwardIos } from "react-icons/md";
+import '../styles/home.css'
+
+function AboutSlideShow() {
+  return (
+    <>
+
+
+        {/* <div className='border-2 border-solid border-black w-full h-full'></div>
+        <div className='border-2 border-solid border-black w-full h-full'></div>
+        <div className='border-2 border-solid border-black w-full h-full'></div> */}
+
+        <div className='border-2 border-solid border-red-500 w-[100px] h-[100px] absolute bottom-1/2 left-1/2'>
+            <span className='dot'></span>
+            <span className='dot'></span>
+            <span className='dot'></span>
+        </div>
+
+    </>
+  )
+}
+
+export default AboutSlideShow
