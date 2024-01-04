f6a2bd1082f7f7f8e59c843707ac84b0f745de56
SABA HAILU <sabahailu@SABAs-MacBook-Air.local>
2024-01-04 08:07:15 -0500

    media queries inline

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -22,7 +22,7 @@ function About() {
               <div id="yama-logo-left" className='w-[5000px] h-full bg-yama bg-[length:100px_100px] bg-center relative translate-x-full'>
               </div>
             </div>
-            <p className='leading-[50px] text-[18px] h-1/2 px-16 text-justify font-normal mt-8'>Welcome to the YAMA Anime and Manga Association, a vibrant community committed to sharing our love of anime, manga, and beyond. Our mission is to create a friendly and inclusive atmosphere where like-minded fans may connect and share their passions. At YAMA, we take pleasure in providing a lively setting for our members through our wide variety of activities and events. Join YAMA today to make new friends and fun memories!</p>
+            <p className='leading-[50px] xl:text-[18px] lg:text-[18px] text-[15px] h-1/2 px-16 text-justify font-normal mt-8'>Welcome to the YAMA Anime and Manga Association, a vibrant community committed to sharing our love of anime, manga, and beyond. Our mission is to create a friendly and inclusive atmosphere where like-minded fans may connect and share their passions. At YAMA, we take pleasure in providing a lively setting for our members through our wide variety of activities and events. Join YAMA today to make new friends and fun memories!</p>
             <div className='w-full h-[90px] self-center mb-4'>
               <div id="yama-logo-right" className='w-[5000px] h-full bg-yama bg-[length:100px_100px] bg-center relative translate-x-full'>
               </div>
@@ -31,9 +31,9 @@ function About() {
           <AboutSlideShow />
         </section>
         <section id="membership" className='flex lg:flex-row lg:justify-between flex-col first-section w-full h-[800px] items-center z-20'>
-          <div className='w-full h-full flex flex-col justify-end items-center text-white bg-black opacity-[0.95]'>
-            <h1 className='bounce text-[70px] font-bold'>Receive Your Membership Today!</h1>
-            <div className='flip-container relative w-1/2 h-[650px]'>
+          <div className='w-full h-full flex flex-col xl:justify-end lg:justify-end justify-evenly items-center text-white bg-black opacity-[0.95] text-center'>
+            <h1 className='w-full bounce xl:text-[70px] md:text-[50px] text-[35px] font-bold'>Receive Your Membership Today!</h1>
+            <div className='flip-container relative xl:w-1/2 xl:h-[650px] lg:w-1/2 lg:h-[650px] w-full h-[500px]'>
               <div className='flip-card absolute w-full h-full transition-all duration-700 ease-linear hover:rotate-180'>
                 <div className='flip-front absolute w-full h-full'>
                   <img src={membership} className='w-full h-full'/>
@@ -75,9 +75,9 @@ function About() {
           </a>
           </div>
         </div>
-        <div id="third-paragraph" className='w-full lg:h-[673px] flex justify-between items-center z-20'>
-          <div id="register-paragraph" className='w-1/2 h-full flex justify-center items-center bg-tabling bg-contain relative'>
-            <ul id="register-list" className='w-1/2 h-[400px] z-20 text-[18px] flex flex-col justify-evenly items-start list-disc border-2 border-solid border-white/80 relative font-bold px-8 lg:ml-4'>
+        <div id="third-paragraph" className='w-full xl:h-[700px] lg:h-[700px] h-[1000px] xl:flex lg:flex xl:justify-between lg:justify-between justify-center items-center z-20'>
+          <div id="register-paragraph" className='xl:w-1/2 lg:w-1/2 w-full xl:h-full lg:h-full h-1/2 flex justify-center items-center bg-tabling bg-cover relative'>
+            <ul id="register-list" className='w-1/2 h-[400px] z-20 text-[18px] flex flex-col justify-evenly items-start list-disc relative font-bold px-8 lg:ml-4'>
               <h3 className='text-[25px] z-20 underline self-center'>Registration</h3>
               <li>Full Year Membership - <span className='text-red-500'>$15</span></li>
               <li>Single Semester Membership - <span className='text-red-500'>$10</span></li>
@@ -85,7 +85,7 @@ function About() {
               <li>Registration Updates Made in our Discord Server!</li>
             </ul>
           </div>
-          <div id="contact-paragraph" className='w-1/2 h-full flex justify-center items-center relative bg-contact bg-cover bg-orange-400'>
+          <div id="contact-paragraph" className='xl:w-1/2 lg:w-1/2 w-full xl:h-full lg:h-full h-1/2 flex justify-center items-center relative bg-contact bg-cover bg-orange-400'>
             <ul id="contact-list" className='w-3/5 h-4/5 z-20 text-[28px] flex flex-col justify-evenly items-start list-disc relative font-bold px-8 text-white lg:ml-8 m-0'>
               <h3 className='text-[35px] z-20 underline self-center'>Inquiries</h3>
               <li>Email yorkuyamako@gmail.com to address any questions</li>
